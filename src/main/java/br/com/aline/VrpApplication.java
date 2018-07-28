14e45f8230e5bdad0a7b414fdf99aa7c20955a3b
Aline Dias <aline@segdig.com.br>
2018-07-28 14:10:26 -0300

    first commit

---
+++ src/main/java/br/com/aline/VrpApplication.java
@@ -0,0 +1,14 @@
+package br.com.aline;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrpApplication.class, args);
+    }
+
+}
